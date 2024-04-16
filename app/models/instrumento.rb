e45b22f86f1fe96ed9a17b0a2ea0091dbee7a4a6
francksefu <francksefu1998@gmail.com>
2024-04-16 09:24:49 +0200

    Add validation

--- app/models/instrumento.rb
+++ app/models/instrumento.rb
@@ -1,2 +1,7 @@
 class Instrumento < ApplicationRecord
+  validates :pergunta_um, presence: true
+  validates :pergunta_dois, presence: true
+  validates :pergunta_tres, presence: true
+  validates :pergunta_quatro, presence: true
+  validates :pergunta_cinco, presence: true
 end
