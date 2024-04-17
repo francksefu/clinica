50f7600547a47170f03ceba79134bb140a13d272
francksefu <francksefu1998@gmail.com>
2024-04-17 04:05:02 +0200

    Update readme file

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -37,16 +37,15 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.perform_caching = false
   config.action_mailer.delivery_method = :smtp
-config.action_mailer.smtp_settings = {
-  address:              'smtp.yourserver.com',
-  port:                 25,
-  domain:               'yourrubyapp.com',
-  user_name:            '<your username>',
-  password:             '<your password>',
-  authentication:       'plain',
-  enable_starttls_auto: true
-}
+  config.action_mailer.smtp_settings = {
+    address:         'smtp.gmail.com',
+    port:            587,
+     }
 
 
   
